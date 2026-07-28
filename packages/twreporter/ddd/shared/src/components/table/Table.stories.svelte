58bb7df002068da0ca5d615600e0b2d63d181dc4
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-07-28 10:28:06 +0800

    chore(shared/table): add example configs

--- packages/twreporter/ddd/shared/src/components/table/Table.stories.svelte
+++ packages/twreporter/ddd/shared/src/components/table/Table.stories.svelte
@@ -5,7 +5,11 @@
   const { Story } = defineMeta({
     title: 'Components/Table',
     component: Table,
-    args: { src: '', config: '' },
+    args: {
+      src: 'https://projects.twreporter.org/twreporter/ddd/2026-07-transnational-repression/1.csv',
+      config:
+        'https://projects.twreporter.org/twreporter/ddd/2026-07-transnational-repression/1.json',
+    },
     argTypes: {
       src: { control: 'text' },
       config: { control: 'text' },
